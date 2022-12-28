9c88c6f73b65ae2de304ec2d29289597a4b62fcb
AarhamH <aarham.haider@gmail.com>
2022-12-27 21:19:35 -0800

    Destroyed Starter Files and Incorporated Scratch Site Components
    - deleted premade index.html, manifest.json, robot.txt from public
    
    - deleted App.jsx, App.css, App.js from src
    
    - uploaded site components, such as header, footer, portfolio with a .jsx and .css file respectively
    
    - began filling header portion

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+        <a href="https://www.linkedin.com/in/aarham-haider-5a0a4a1bb" target="_blank"></a>
+        <a href="https://github.com/AarhamH" target="_blank"></a>
+        <a href="https://aarhamh.itch.io/" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
