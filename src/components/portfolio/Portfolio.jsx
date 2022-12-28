afbb77cc94ab5b05f6f49996346628452d42e532
AarhamH <aarham.haider@gmail.com>
2022-12-28 15:39:39 -0800

    Added Portfolio/Projects section
    implemented components for Portfolio.jsx and portfolio.css to display gifs of my projects and their github/live demo links

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/ball-dodger-gif.gif'
+import IMG2 from '../../assets/dungeon-crawlers.gif'
+
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>These are my works</h5>
+      <h2>Projects</h2>
+
+      <div className="container portfolio_container">
+        <article className='portfolio_items'>
+          <div className="portfolio_items-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3> Ball Dodgers</h3>
+          <div className="portfolio_items-cta">
+            <a href="https://github.com/AarhamH/ball-dodger" className='btn' target='_blank'>Github</a>
+            <a href="https://aarhamh.itch.io/ball-dodgers" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio_items'>
+          <div className="portfolio_items-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3> Pichi Dungeon Crawlers</h3>
+          <div className="portfolio_items-cta">
+            <a href="https://github.com/AarhamH/ball-dodger" className='btn' target='_blank'>Github</a>
+            <a className='btn btn-primary' target='_blank'>COMING SOON</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
