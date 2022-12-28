28a181e6f47b5b68fb7e355fea11fadca791c8d1
AarhamH <aarham.haider@gmail.com>
2022-12-27 21:21:51 -0800

    Installed react-icons package
    - installed react icons package to add github, linkedin and itchio logos as links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FaItchIo} from 'react-icons/fa'
 
 
 
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://www.linkedin.com/in/aarham-haider-5a0a4a1bb" target="_blank"></a>
-        <a href="https://github.com/AarhamH" target="_blank"></a>
-        <a href="https://aarhamh.itch.io/" target="_blank"></a>
+        <a href="https://www.linkedin.com/in/aarham-haider-5a0a4a1bb" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/AarhamH" target="_blank"><FaGithub/></a>
+        <a href="https://aarhamh.itch.io/" target="_blank"><FaItchIo/></a>
     </div>
   )
 }
