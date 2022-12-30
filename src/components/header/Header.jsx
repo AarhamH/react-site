a6b733e4200fb66589d2b9fe54a50831dcc5b7b5
AarhamH <aarham.haider@gmail.com>
2022-12-30 04:33:06 -0800

    Implemented Typewriter effect
    -installed and incorporated react package to type out name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,20 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
+import Typist from 'react-typist'
 
 const Header = () => {
   return (
     <header>
       <div className="container header_container">
         <h5> Hey I'm</h5>
-        <h1>Aarham <span>Haider</span></h1>
+        <div className='header_name'>
+            <Typist cursor={{blink: true, show: true, hideWhenDone: true}}>
+              <Typist.Delay ms={1000}/>
+              Aarham <span>Haider</span>
+            </Typist>
+
+        </div>
         <h5 className="text-light"> Developer</h5>
         <CTA />
         <HeaderSocials/>
