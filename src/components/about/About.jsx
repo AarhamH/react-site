895beb9622aee9bfec1fa3dca3c9b88d9d5426e5
AarhamH <aarham.haider@gmail.com>
2023-01-30 17:50:21 -0800

    Deleted commented section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,27 +23,6 @@ const About = () => {
         </div>
 
         <div className="about_content">
-          {/*
-          <div className="about_cards">
-            <article className='about_card'>
-              <MdSchool className='about_icon'/>
-              <h5>Education</h5>
-              <small>SFU BSc of Science in Computer Science (current)</small>
-            </article>
-        
-            <article className='about_card'>
-              <FaAward className='about_icon'/>
-              <h5>Experience</h5>
-              <small>800+ hrs working,<br></br>1400+ hrs volunteer</small>
-            </article>
-
-            <article className='about_card'>
-              <MdGamepad className='about_icon'/>
-              <h5>Game Development</h5>
-              <small>1 complete <br></br> 1 in-development</small>
-            </article>
-          </div>
-        */}
           <p>My name is Aarham Haider and I am interested in creating various things. I am currently a Computer Science student 
             at Simon Fraser University, where I was able to study and develop expertise in various CS skills and conventions. I am
             also an aspiring video game developer and I have started production on two larger scale game projects during the summer of 2021.
