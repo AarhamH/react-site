dc9eb0085c70630cdaf4e76453688c08ada7cf01
AarhamH <aarham.haider@gmail.com>
2023-02-13 02:44:25 -0800

    Test deployment

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import CV from '../../assets/Aarham-Haider-Resume.pdf'
+import CV from '../../assets/Resume.pdf'
 import AOS from 'aos'
 import "aos/dist/aos.css"
 
